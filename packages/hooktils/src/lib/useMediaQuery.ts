a978b3893b6d4a67b6a56d9ca874d66b091165a1
Matias Navarro <matiasg.developer@gmail.com>
2023-07-09 00:00:20 -0300

    feat: add usemediaquery

---
+++ packages/hooktils/src/lib/useMediaQuery.ts
@@ -0,0 +1,25 @@
+import { useLayoutEffect, useState } from "react";
+
+export function useMediaQuery(query: string): boolean {
+  const [matches, setMatch] = useState(() => {
+    const supportMatchMedia =
+      typeof window !== "undefined" && typeof window.matchMedia !== "undefined";
+    if (supportMatchMedia) {
+      return window.matchMedia(query).matches;
+    }
+    return false;
+  });
+
+  useLayoutEffect(() => {
+    const queryList = matchMedia(query);
+    const handler = (e: MediaQueryListEvent) => {
+      setMatch(e.matches);
+    };
+    queryList.addEventListener("change", handler);
+    return () => {
+      queryList.removeEventListener("change", handler);
+    };
+  }, [query]);
+
+  return matches;
+}
