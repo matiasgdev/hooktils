1ccc2822cb7420433416df689164127d0b19dfcd
Matias Navarro <matiasg.developer@gmail.com>
2023-07-21 22:36:18 -0300

    refactor: move reset at the top and use on cleaner function

--- packages/hooktils/src/lib/useDebounce.ts
+++ packages/hooktils/src/lib/useDebounce.ts
@@ -4,6 +4,10 @@ export function useDebounce<T>(value: T, delay = 500) {
   const timerRef = useRef<NodeJS.Timeout>();
   const [debounced, setDebouncedValue] = useState<T>(value);
 
+  const reset = useCallback(() => {
+    clearTimeout(timerRef.current);
+  }, []);
+
   useEffect(() => {
     timerRef.current = setTimeout(() => {
       setDebouncedValue(value);
@@ -11,14 +15,10 @@ export function useDebounce<T>(value: T, delay = 500) {
 
     return () => {
       if (timerRef.current !== undefined) {
-        clearTimeout(timerRef.current);
+        reset();
       }
     };
-  }, [value, delay]);
-
-  const reset = useCallback(() => {
-    clearTimeout(timerRef.current);
-  }, []);
+  }, [value, delay, reset]);
 
   return [debounced, reset];
 }
